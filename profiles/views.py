004d239a06ba8b7e603d0333e77298eab8b22571
JacquesIversen <jackiracki1@gmail.com>
2023-10-14 14:21:34 +0000

    Added profile,views, urls and serializers, all checked and running correct data:

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
-class Profile
+""" Serializer for det user Profile """
+class ProfileView(APIView):
+    
+    def get(self, request):
+        profileObjects = Profile.objects.all()
+        serializer = ProfileSerializer(profileObjects, many=True)
+        return Response(serializer.data)
