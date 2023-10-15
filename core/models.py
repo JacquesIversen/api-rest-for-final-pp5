89ba293b8355530f43ebc4f4297938c6ec5a9bfd
JacquesIversen <jackiracki1@gmail.com>
2023-10-15 13:50:12 +0000

    models added in core for models/issues, alongside serializers.

--- core/models.py
+++ core/models.py
@@ -1,2 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+""" This sections holds the models for the Issues posted by the user:  """
+class Issue(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    car = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    engine_size = models.DecimalField(max_digits=3, decimal_places=1)
+    description = models.TextField(blank=True)
+    is_solved = models.BooleanField(default=True)
+    image = image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
---
+++ core/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Issue
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+    
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Issue
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at',
+            'title', 'car', 'model', 'year', 'engine_size',
+            'description', 'is_solved', 'image',
+        ]
