5275ce02862228eaf8611a6015f3315cc5ea536c
JacquesIversen <jackiracki1@gmail.com>
2023-10-16 14:36:01 +0000

    profile and issue views refactored to follow same generic and permission model as like and dislike

--- core/views.py
+++ core/views.py
@@ -1,64 +1,25 @@
 from pp5_api.permissions import IsOwnerOrReadOnly
 from .models import Issue, Comment, Like, DisLike
 from .serializers import IssueSerializer, CommentSerializer, CommentDetailSerializer, DisLikeSerializer, LikeSerializer
-from django.http import Http404
 from rest_framework import status, permissions, generics
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
-class IssueView(APIView):
+
+""" Views for IssueList/View  """
+class IssueView(generics.ListCreateAPIView):
     serializer_class = IssueSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
-    
-    def get(self, request):
-        issues = Issue.objects.all()
-        serializer = IssueSerializer(
-            issues, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Issue.objects.all()
 
-    def post(self, request):
-        serializer = IssueSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class IssueDetail(APIView):
+class IssueDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = IssueSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        try:
-            issue = Issue.objects.get(pk=pk)
-            self.check_object_permissions(self.request, issue)
-            return issue
-        except Issue.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        issue = self.get_object(pk)
-        serializer = IssueSerializer(
-            issue, context={'request': request}
-        )
-        return Response(serializer.data)
+    queryset = Issue.objects.all()
 
-    def put(self, request, pk):
-        issue = self.get_object(pk)
-        serializer = IssueSerializer(
-            issue, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
