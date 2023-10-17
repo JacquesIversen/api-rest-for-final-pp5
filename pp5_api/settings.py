102a24d6650e47b6d06a20340a2c0f6bad69a9b7
JacquesIversen <jackiracki1@gmail.com>
2023-10-17 20:45:47 +0000

    changed default Auth.

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -33,9 +33,6 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_AUTHENTICATION_CLASSES': [(
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
