c2ba7843b8ecdb3bc6d5f42f132d942b993b48dd
JacquesIversen <jackiracki1@gmail.com>
2023-10-17 08:48:55 +0000

    added Client origin and Allowed host to settings

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -66,7 +67,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 """ DEBUG = 'DEV' in os.environ """
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jacquesiver-apirestforf-t7kxfxwr0io.ws-eu105.gitpod.io', 'https://pp5-api-final-b2f167f1cdb6.herokuapp.com', 'pp5-api-final-b2f167f1cdb6.herokuapp.com']
+ALLOWED_HOSTS = ['8000-jacquesiver-apirestforf-t7kxfxwr0io.ws-eu105.gitpod.io', os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
@@ -111,13 +112,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
