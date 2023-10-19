8f7a7ed88a55eefd8363483b2d9e6a20a40f7644
JacquesIversen <jackiracki1@gmail.com>
2023-10-19 19:14:57 +0000

    line 59 in settings

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -52,7 +52,7 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'pp5-api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'pp5_api.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
