79970129daf532eaea5241eef71eae0debae4792
JacquesIversen <jackiracki1@gmail.com>
2023-10-21 16:46:11 +0000

    Axios req is useless

--- pp5_api/views.py
+++ pp5_api/views.py
@@ -1,10 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
+from .settings import REST_AUTH
 
 
 @api_view()
@@ -14,17 +10,18 @@ def root_route(request):
     })
 
 
+""" # dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
     response.set_cookie(
-        key=JWT_AUTH_COOKIE,
+        key='my-app-auth',
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite='None',
+        secure=True,
     )
     response.set_cookie(
         key=JWT_AUTH_REFRESH_COOKIE,
@@ -32,7 +29,7 @@ def logout_route(request):
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite='None',
+        secure=True,
     )
-    return response
+    return response """
