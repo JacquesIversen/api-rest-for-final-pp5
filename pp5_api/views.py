7202e5364b9a064c343effda72dd3ebb22d3cf15
JacquesIversen <jackiracki1@gmail.com>
2023-10-21 16:40:30 +0000

    changed views to JWT

--- pp5_api/views.py
+++ pp5_api/views.py
@@ -1,6 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import REST_AUTH
+from rest_framework.renderers import JSONRenderer
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
 
 
 @api_view()
@@ -10,18 +14,17 @@ def root_route(request):
     })
 
 
-# dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
     response = Response()
     response.set_cookie(
-        key='my-app-auth',
+        key=JWT_AUTH_COOKIE,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite='None',
-        secure=True,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
     )
     response.set_cookie(
         key=JWT_AUTH_REFRESH_COOKIE,
@@ -29,7 +32,7 @@ def logout_route(request):
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite='None',
-        secure=True,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
     )
     return response
