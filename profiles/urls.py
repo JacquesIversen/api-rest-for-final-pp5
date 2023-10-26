f89a28652d6f914131468d10d4aa7fbe8918e6eb
JacquesIversen <jackiracki1@gmail.com>
2023-10-26 20:12:24 +0000

    indentation errer

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from profiles import views
 
 urlpatterns = [
    path('profiles/', views.ProfileView.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
-    path('me/', views.MyProfile.as_view()),
+   path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
+   path('me/', views.MyProfile.as_view()),
 ]
